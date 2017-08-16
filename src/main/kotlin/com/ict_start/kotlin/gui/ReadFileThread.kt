2f8e3da87e6026ca91137fdb683b2b1e7e5da4c9
Takuya Horimatsu <new_woory@hotmail.com>
2017-08-16 18:25:02 +0900

    indent change

--- src/main/kotlin/com/ict_start/kotlin/gui/ReadFileThread.kt
+++ src/main/kotlin/com/ict_start/kotlin/gui/ReadFileThread.kt
@@ -7,65 +7,65 @@ import java.lang.Thread
 import java.util.concurrent.TimeUnit
 
 class ReadFileThread(var filePath: String) : Thread() {
-    private var updatedLines = mutableListOf<String>()
-    var isReading = true
-    val InitLines = 5
+  private var updatedLines = mutableListOf<String>()
+  var isReading = true
+  val InitLines = 5
 
-    override fun run() {
-        val lineBuffer = arrayOfNulls<String>(InitLines)
-        var arrayAmount:Long = 0
+  override fun run() {
+    val lineBuffer = arrayOfNulls<String>(InitLines)
+    var arrayAmount:Long = 0
 
-        println("Started")
-        isReading = true
-        val br = BufferedReader(FileReader(File(filePath)))
+    println("Started")
+    isReading = true
+    val br = BufferedReader(FileReader(File(filePath)))
 
-        while(true) {
-            val line: String = br.readLine() ?: break
-            lineBuffer[(arrayAmount % InitLines).toInt()] = line
-            arrayAmount++
+    while(true) {
+      val line: String = br.readLine() ?: break
+      lineBuffer[(arrayAmount % InitLines).toInt()] = line
+      arrayAmount++
+    }
+    synchronized(updatedLines) {
+      if (arrayAmount < InitLines) {
+        for (i in (0..arrayAmount - 1)) {
+          val line = lineBuffer[i.toInt()]
+          if (line != null) {
+            updatedLines.add(line)
+          }
         }
+      } else {
+        for (i in ((arrayAmount - InitLines)..arrayAmount - 1)) {
+          val index = (i % InitLines).toInt()
+          val line = lineBuffer[index]
+          if (line != null) {
+            updatedLines.add(line)
+          }
+        }
+      }
+    }
+
+    while(isReading) {
+      val line: String? = br.readLine()
+      if (line == null) {
+        TimeUnit.MILLISECONDS.sleep(500);
+      } else {
         synchronized(updatedLines) {
-            if (arrayAmount < InitLines) {
-                for (i in (0..arrayAmount - 1)) {
-                    val line = lineBuffer[i.toInt()]
-                    if (line != null) {
-                        updatedLines.add(line)
-                    }
-                }
-            } else {
-                for (i in ((arrayAmount - InitLines)..arrayAmount - 1)) {
-                    val index = (i % InitLines).toInt()
-                    val line = lineBuffer[index]
-                    if (line != null) {
-                        updatedLines.add(line)
-                    }
-                }
-            }
+          updatedLines.add(line)
         }
-
-        while(isReading) {
-            val line: String? = br.readLine()
-            if (line == null) {
-                TimeUnit.MILLISECONDS.sleep(500);
-            } else {
-                synchronized(updatedLines) {
-                    updatedLines.add(line)
-                }
-            }
-        }
-        println("Finished")
+      }
     }
+    println("Finished")
+  }
 
-    fun hasUpdate(): Boolean{
-        return updatedLines.size > 0
-    }
+  fun hasUpdate(): Boolean{
+    return updatedLines.size > 0
+  }
 
-    fun getUpdated(): MutableList<String> {
-        val returnList = mutableListOf<String>()
-        synchronized(updatedLines) {
-            returnList.addAll(updatedLines)
-            updatedLines.clear()
-        }
-        return returnList
+  fun getUpdated(): MutableList<String> {
+    val returnList = mutableListOf<String>()
+    synchronized(updatedLines) {
+      returnList.addAll(updatedLines)
+      updatedLines.clear()
     }
+    return returnList
+  }
 }
--- src/main/kotlin/com/ict_start/kotlin/gui/StreamLogGuiMain.kt
+++ src/main/kotlin/com/ict_start/kotlin/gui/StreamLogGuiMain.kt
@@ -11,92 +11,92 @@ import javafx.stage.Stage
 import java.util.*
 
 class StreamLogGuiMain : Application() {
-    private var readFilePath: TextField = TextField("")
-    private var eventStartButton: Button = Button("Start")
-    private var clearButton: Button = Button("Clear")
-    private var closeButton: Button = Button("Close")
-    private var readThread: ReadFileThread? = null
-    private var logConsole: TextArea = TextArea("")
-    val period = 100L
+  private var readFilePath: TextField = TextField("")
+  private var eventStartButton: Button = Button("Start")
+  private var clearButton: Button = Button("Clear")
+  private var closeButton: Button = Button("Close")
+  private var readThread: ReadFileThread? = null
+  private var logConsole: TextArea = TextArea("")
+  val period = 100L
 
-    override fun start(primaryStage: Stage) {
-        primaryStage.title = "Log Read"
+  override fun start(primaryStage: Stage) {
+    primaryStage.title = "Log Read"
 
-        val args: MutableList<String> = parameters.raw
-        readFilePath.text = if(args.size > 0) args[0] else ""
+    val args: MutableList<String> = parameters.raw
+    readFilePath.text = if(args.size > 0) args[0] else ""
 
-        val pane = BorderPane()
-        val buttonPane = BorderPane()
-        buttonPane.left = eventStartButton
-        buttonPane.center = clearButton
-        buttonPane.right = closeButton
+    val pane = BorderPane()
+    val buttonPane = BorderPane()
+    buttonPane.left = eventStartButton
+    buttonPane.center = clearButton
+    buttonPane.right = closeButton
 
-        pane.top = readFilePath
-        pane.center = logConsole
-        pane.bottom = buttonPane
-        val scene = Scene(pane, 600.0, 400.0)
-        logConsole.isWrapText = true
+    pane.top = readFilePath
+    pane.center = logConsole
+    pane.bottom = buttonPane
+    val scene = Scene(pane, 600.0, 400.0)
+    logConsole.isWrapText = true
 
-        eventStartButton.setOnMouseClicked {
+    eventStartButton.setOnMouseClicked {
+      val threadIsReading = readThread?.isReading ?: false
+              && readThread?.isAlive ?: false
+
+      if (threadIsReading) {
+        readThread?.isReading = false
+      } else {
+        logConsole.text = ""
+        val filePath: String = readFilePath.text ?: ""
+        if (!filePath.isEmpty()) {
+          eventStartButton.text = "Started."
+
+          readThread = ReadFileThread(filePath)
+          readThread?.start()
+        }
+      }
+    }
+    clearButton.setOnMouseClicked {
+        logConsole.text = ""
+    }
+    closeButton.setOnMouseClicked {
+      if (readThread != null) {
+        readThread?.isReading = false
+      }
+      Platform.exit()
+      System.exit(0)
+    }
+    primaryStage.scene = scene
+    primaryStage.show()
+
+    val timer = Timer()
+
+    timer.schedule(object : TimerTask() {
+      override fun run() {
+        Platform.runLater({
+          if (readThread != null) {
             val threadIsReading = readThread?.isReading ?: false
                     && readThread?.isAlive ?: false
-
             if (threadIsReading) {
-                readThread?.isReading = false
-            } else {
-                logConsole.text = ""
-                val filePath: String = readFilePath.text ?: ""
-                if (!filePath.isEmpty()) {
-                    eventStartButton.text = "Started."
-
-                    readThread = ReadFileThread(filePath)
-                    readThread?.start()
+              if (readThread?.hasUpdate() ?: false) {
+                val list = readThread?.getUpdated()
+                if (list != null) {
+                  for (line:String in list) {
+                    logConsole.appendText(line)
+                    logConsole.appendText("\n")
+                  }
                 }
+              }
+            } else {
+              readThread?.isReading = false
+              readThread = null
+              eventStartButton.text = "Click"
             }
-        }
-        clearButton.setOnMouseClicked {
-            logConsole.text = ""
-        }
-        closeButton.setOnMouseClicked {
-            if (readThread != null) {
-                readThread?.isReading = false
-            }
-            Platform.exit()
-            System.exit(0)
-        }
-        primaryStage.scene = scene
-        primaryStage.show()
-
-        val timer = Timer()
-
-        timer.schedule(object : TimerTask() {
-            override fun run() {
-                Platform.runLater({
-                    if (readThread != null) {
-                        val threadIsReading = readThread?.isReading ?: false
-                                && readThread?.isAlive ?: false
-                        if (threadIsReading) {
-                            if (readThread?.hasUpdate() ?: false) {
-                                val list = readThread?.getUpdated()
-                                if (list != null) {
-                                    for (line:String in list) {
-                                        logConsole.appendText(line)
-                                        logConsole.appendText("\n")
-                                    }
-                                }
-                            }
-                        } else {
-                            readThread?.isReading = false
-                            readThread = null
-                            eventStartButton.text = "Click"
-                        }
-                    }
-                })
-            }
-        }, 0, period)
-    }
+          }
+        })
+      }
+    }, 0, period)
+  }
 }
 
 fun main(args: Array<String>) {
-    Application.launch(StreamLogGuiMain::class.java, *args)
+  Application.launch(StreamLogGuiMain::class.java, *args)
 }
